290cb3a9fcc87579f957646a1e0fd757e47ecb15
elalfymohamed <elalfy.mohamed19@gmail.com>
2021-10-03 00:38:24 +0200

    set icon data in the file

---
+++ src/data/FilterStar.js
@@ -0,0 +1,91 @@
+import { BsStarFill } from "react-icons/bs";
+import { FiStar } from "react-icons/fi";
+
+export const star = [
+  {
+    id: 1,
+    starIcon: [
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+    ],
+
+    rating: 5,
+  },
+  {
+    id: 2,
+    starIcon: [
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <FiStar />,
+      },
+    ],
+
+    rating: 4,
+  },
+  {
+    id: 3,
+    starIcon: [
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <FiStar />,
+      },
+      {
+        icon: <FiStar />,
+      },
+    ],
+    rating: 2,
+  },
+  {
+    id: 4,
+    starIcon: [
+      {
+        icon: <BsStarFill />,
+      },
+      {
+        icon: <FiStar />,
+      },
+      {
+        icon: <FiStar />,
+      },
+      {
+        icon: <FiStar />,
+      },
+      {
+        icon: <FiStar />,
+      },
+    ],
+    rating: 1,
+  },
+];
